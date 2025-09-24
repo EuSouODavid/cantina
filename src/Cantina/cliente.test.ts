617dc42897818e0a8da628a9898ccb065bae34b8
EuSouODavid <davidkurzlol@gmail.com>
2025-09-24 02:45:14 -0300

    finalizado o teste de cliente

--- src/Cantina/cliente.test.ts
+++ src/Cantina/cliente.test.ts
@@ -11,7 +11,7 @@ describe("Quando manipular Cliente", ()=>{
         const telValido = tel.validarTelefone(tel.telefone);
         
         expect(telValido).toBe(true);
-       
+
     })
     test("deve invalidar um número de telefone com mais ou menos de 11 dígitos", ()=>{
         const NTelefone ="123456";
@@ -25,13 +25,26 @@ describe("Quando manipular Cliente", ()=>{
     test("deve atualizar o telefone se o novo número for válido",()=>{
         const NTelefone ="53981560341";
         const tel: Cliente = new Cliente;
-        tel.telefone = NTelefone
+        tel.telefone = NTelefone;
+        
+        const novoTelefone = "53999998888";
+        tel.atualizarTelefone(novoTelefone);
 
-        const telValido = tel.validarTelefone(tel.telefone);
-        const novoTelefone= tel.atualizarTelefone("53999998888")
-
-        expect(novoTelefone).toBe("53999998888")
+        expect(tel.telefone).toBe("53999998888")
     })
-
+    test("deve lançar um erro ao tentar atualizar com um telefone inválido",()=>{
+        const NTelefone ="53981560341";
+        const tel: Cliente = new Cliente;
+        tel.telefone = NTelefone;
+        
+        expect(() => tel.atualizarTelefone("5399999888")).toThrow("Telefone Inválido");
+    })
+    test("deve lançar um erro ao tentar criar um cliente com nome inválido (menos de 3 caracteres)",()=>{
+        const NTelefone ="53981560341";
+        const tel: Cliente = new Cliente;
+        tel.telefone = NTelefone;
+        
+        expect(() => tel.criarCliente("Al", "53981560341")).toThrow("Nome inválido");
+    })
 })
 
